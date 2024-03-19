32b31f79fa171c2255d6c256f0593ca04cd710af
alfiyyy <ianalfianyusuf@gmail.com>
2024-03-20 01:19:29 +0700

    seccond commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:latihan_kuis_praktikum_mobile/screens/home.dart';
 import 'package:latihan_kuis_praktikum_mobile/screens/login.dart';
 
 void main() {
@@ -11,7 +12,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: LoginScreen(),
+      home: Home(),
     );
   }
 }
--- lib/screens/detail.dart
+++ lib/screens/detail.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:latihan_kuis_praktikum_mobile/data/data.dart';
 
-
 class Detail extends StatefulWidget {
   final TourismPlace detail;
 
@@ -16,60 +15,65 @@ class _DetailState extends State<Detail> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          leading: IconButton(onPressed: () {Navigator.of(context).pop();}, icon: Icon(Icons.arrow_back_ios_new_rounded)),
+          leading: IconButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              icon: Icon(Icons.arrow_back_ios_new_rounded)),
           backgroundColor: Colors.grey.shade300,
           title: Text(
             widget.detail.name,
-            style: TextStyle(color: Colors.black), 
+            style: TextStyle(color: Colors.black),
           ),
         ),
         body: Column(
           children: [
-            SizedBox(height: 20,),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Column(
-                  children: [
-                    Image.network(
-                      widget.detail.imageUrls[0],
-                      height: 100,
-                      width: 100,
-                      fit: BoxFit.cover,
-                    ),
-                    Icon(Icons.access_time_outlined),
-                    Text(widget.detail.openDays),
-                  ],
-                ),
-                Column(
-                  children: [
-                    Image.network(
-                      widget.detail.imageUrls[1],
-                      height: 100,
-                      width: 100,
-                      fit: BoxFit.cover,
-                    ),
-                    Icon(Icons.access_time_filled_outlined),
-                    Text(widget.detail.openTime),
-                  ],
-                ),
-                Column(
-                  children: [
-                    Image.network(
-                      widget.detail.imageUrls[2],
-                      height: 100,
-                      width: 100,
-                      fit: BoxFit.cover,
-                    ),
-                    Icon(Icons.money),
-                    Text(widget.detail.ticketPrice),
-                  ],
-                ),
-              ],
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              height: 160,
+              child: Expanded(
+                  child: ListView.builder(
+                      itemCount: 3,
+                      scrollDirection: Axis.horizontal,
+                      itemBuilder: ((context, index) {
+                        return Column(
+                          children: [
+                            Image.network(
+                              widget.detail.imageUrls[index],
+                              height: 100,
+                              width: 200,
+                            ),
+                            (index == 0)
+                                ? Column(
+                                    children: [
+                                      Icon(Icons.access_time_outlined),
+                                      Text(widget.detail.openDays)
+                                    ],
+                                  )
+                                : (index == 1)
+                                    ? Column(
+                                        children: [
+                                          Icon(Icons
+                                              .access_time_filled_outlined),
+                                          Text(widget.detail.openTime)
+                                        ],
+                                      )
+                                    : (index == 2)
+                                        ? Column(
+                                            children: [
+                                              Icon(Icons.money),
+                                              Text(widget.detail.ticketPrice),
+                                            ],
+                                          )
+                                        : SizedBox()
+                          ],
+                        );
+                      }))),
             ),
             Text(widget.detail.description),
           ],
         ));
   }
 }
-
